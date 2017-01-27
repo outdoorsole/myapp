4c152ce95bf7539c54d1ebcacb8c3747a83e8634
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-01-27 14:27:44 -0800

    Add start script to package.json.

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.get('/greetings/:name', function(req, res) {
 });
 
 // SERVER
-
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Server listening on port: ' + port);
