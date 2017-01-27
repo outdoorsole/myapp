5bcd7091c144f2f63a443aa1c3ae585f607f3c4f
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-01-27 11:49:15 -0800

    Add /api/blahs route to send back an array of objects.

--- app.js
+++ app.js
@@ -10,9 +10,16 @@ app.use(express.static('public'));
 // ROUTES
 app.get('/', function(req, res) {
   res.render('home');
-})
+});
+
+app.get('/api/blahs', function(req, res) {
+  res.json(
+    [
+      { "name" : "blah" }, { "name" : "blah" }, { "name": "blah" }
+    ]);
+});
 
 // SERVER
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-})
+});
