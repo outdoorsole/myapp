6d37e787db0f73e74e73e0b12ec7236c80a4ea6d
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-01-27 11:29:27 -0800

    Link bootstrap, jQuery.

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ var express = require('express')
 var exphbs = require('express-handlebars')
 var app = express()
 
-app.use(express.static('public'))
+// MIDDLEWARE
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(express.static('public'));
 
+// ROUTES
 app.get('/', function(req, res) {
   res.render('home');
 })
 
+// SERVER
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
