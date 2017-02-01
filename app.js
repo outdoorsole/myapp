32b5bbaceab0ed8e602706b9b386c3f55f2de4eb
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-02-01 10:07:15 -0800

    Add Mongoose & open connection to db.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+// NODE MODULES
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+
 
 // MIDDLEWARE
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+
+/* open a connection to the database */
+mongoose.connect('mongodb://localhost/test');
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we\'re connected!');
+});
+
 
 // ROUTES
 app.get('/', function(req, res) {
